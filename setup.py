a5f22257fca37d522e7f18634912661c4d6b1a48
andersh <anders.haal@ingby.com>
2019-09-05 18:30:30 +0200

    Fixed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='icinga2-exporter',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     author='thenodon',
     author_email='anders@opsdis.com',
