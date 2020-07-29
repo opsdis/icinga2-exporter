d5be67f3ed5ecc7e024fbd4db7270a4ca55bc302
Anders Håål <anders@opsdis.com>
2020-07-29 10:40:04 +0200

    Repackage (#8)
    * Add support for TRAVIS tag and commit env for determine version from tag. Update dependency versions
    
    * dataclasses reverted to 0.6, not found for 3.8
    Update .travis.yml with pip install of wheel
    
    * Limit coverage scope

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import find_packages, setup
+
+
+
+#from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import setup, find_packages
+
+from manage_version import get_version
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
 
 setup(
     name='icinga2-exporter',
-    version='0.1.0',
+    version=get_version(),
     packages=find_packages(),
     author='thenodon',
     author_email='anders@opsdis.com',
@@ -10,4 +23,10 @@ setup(
     license='GPLv3',
     include_package_data=True,
     zip_safe=False,
+    description='A Prometheus exporter for Icinga2',
+    install_requires=read('requirements.txt').split(),
+    #packages=find_packages(where='icinga2-exporter'),
+    #package_dir={'': 'icinga2-exporter'},
+    #  py_modules=[splitext(basename(path))[0] for path in glob('icinga2-exporter/*.py')],
+    python_requires='>=3.6',
 )
