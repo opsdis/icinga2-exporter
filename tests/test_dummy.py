140e107c28a3849a5c6572019d950443183ea84b
andersh <anders.haal@ingby.com>
2021-06-22 13:17:21 +0200

    Add tests

---
+++ tests/test_dummy.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2020  Opsdis AB
+
+    This file is part of mender-boot.
+
+    mender-boot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cmdb2monitor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mender-boot.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import json
+import unittest
+
+
+class DummyTest(unittest.TestCase):
+
+    def test_dummy(self):
+
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
